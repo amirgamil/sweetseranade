37ae377ee28d545a95d1160a01a38eec0099eb26
amirgamil <amir.amgad01@gmail.com>
2023-02-11 17:44:31 -0500

    feat(backend): refine summarize, change to map_reduce

--- server/summarize.py
+++ server/summarize.py
@@ -3,7 +3,7 @@ from langchain.chains.summarize import load_summarize_chain
 from langchain.llms import OpenAI
 from langchain.docstore.document import Document
 
-base_prompt = """Write a detailed summary about {0}, {1} and their relationship. Include specific details about their relationship, interactions, personal details, what they like about each other, and how it changes throughout the story. Also include relevant quotes that could be inserted into the song:"""
+base_prompt = """Write a detailed summary about {0}, {1} and their relationship. Include specific details about their relationship, interactions, personal details, and key events. Also include relevant quotes (marked with quotes) from the context they said or about them:"""
 llm = OpenAI(temperature=0)
 
 
@@ -15,8 +15,11 @@ def summarize_context(character_first: str, character_second: str, contexts: lis
         # so have to construct the prompt template string after we interpolate the characters
         final_prompt = base_prompt.format(character_first, character_second) + "\n{text}\n\nSUMMARY:"
         final_prompt_template = PromptTemplate(template = final_prompt, input_variables=["text"])
-        llm_summarize = load_summarize_chain(llm, chain_type="stuff", prompt=final_prompt_template)
-        return llm_summarize.run(docs)
+        llm_summarize = load_summarize_chain(llm, chain_type="map_reduce", return_intermediate_steps=True, map_prompt=final_prompt_template, combine_prompt=final_prompt_template)
+        # return the last intermediate step since that is more detailed than the summary output
+        res = llm_summarize({"input_documents": docs}, return_only_outputs=True)
+        return res["output_text"]
     except Exception as e:
         print("Error generating completion: ", e)
         raise e
+
