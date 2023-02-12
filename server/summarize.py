e6a4d9bc2f54cd6a623d1f3a9d4b2b30dded2a1a
amir <amir.amgad01@gmail.com>
2023-02-11 19:40:21 -0500

    feat(server): finetune outputs a little, add more detail to summaries (#16)
    * feat(backend): refine summarize, change to map_reduce
    
    * feat(backend): add more details to summarization
    
    * chore(backend): strip whitespaces of context

--- server/summarize.py
+++ server/summarize.py
@@ -1,22 +1,32 @@
 from langchain import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
+from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.docstore.document import Document
 
-base_prompt = """Write a detailed summary about {0}, {1} and their relationship. Include specific details about their relationship, interactions, personal details, what they like about each other, and how it changes throughout the story. Also include relevant quotes that could be inserted into the song:"""
+base_prompt = """Write a detailed summary about {0}, {1} and their relationship. Include specific details about their relationship, interactions, personal details, and key events. Also include relevant quotes (marked with quotes) from the context they said or about them:"""
 llm = OpenAI(temperature=0)
 
 
 
 def summarize_context(character_first: str, character_second: str, contexts: list[str]):
     try:
+        print("starting contexts: ", contexts)
         docs = [Document(page_content=context) for context in contexts]
         # have to do a little weird acrobatics here because summarize cannot take more than one input
         # so have to construct the prompt template string after we interpolate the characters
         final_prompt = base_prompt.format(character_first, character_second) + "\n{text}\n\nSUMMARY:"
         final_prompt_template = PromptTemplate(template = final_prompt, input_variables=["text"])
-        llm_summarize = load_summarize_chain(llm, chain_type="stuff", prompt=final_prompt_template)
-        return llm_summarize.run(docs)
+        llm_summarize = load_summarize_chain(llm, chain_type="map_reduce", return_intermediate_steps=True, map_prompt=final_prompt_template, combine_prompt=final_prompt_template)
+        global_summary = llm_summarize({"input_documents": docs}, return_only_outputs=True)
+
+        # to augment the summary with more details that don't get lost, we extract some info from the summaries
+        doc_summaries = [Document(page_content=summary) for summary in global_summary["intermediate_steps"]]
+        qa_chain = load_qa_chain(llm, chain_type="stuff")
+        query = "What are characteristics that {0} and {1} like about each other and quotes (include quotations) they've said to each other".format(character_first, character_second)
+        additional_context = qa_chain({"input_documents": doc_summaries, "question": query}, return_only_outputs=True)
+        return global_summary["output_text"] + additional_context["output_text"]
     except Exception as e:
-        print("Error generating completion: ", e)
+        print("Error generating summary: ", e)
         raise e
+
