1bd87cc45cf434876f58f39c03d472eaa4fc6e06
verumlotus <universumtremendum@gmail.com>
2023-02-13 13:23:42 -0800

    fix(server): Prevent too large of a global summary input to LLM

--- server/summarize.py
+++ server/summarize.py
@@ -18,13 +18,16 @@ def summarize_context(character_first: str, character_second: str, contexts: lis
         final_prompt_template = PromptTemplate(template = final_prompt, input_variables=["text"])
         llm_summarize = load_summarize_chain(llm, chain_type="map_reduce", return_intermediate_steps=True, map_prompt=final_prompt_template, combine_prompt=final_prompt_template)
         global_summary = llm_summarize({"input_documents": docs}, return_only_outputs=True)
-
-        # to augment the summary with more details that don't get lost, we extract some info from the summaries
-        doc_summaries = [Document(page_content=summary) for summary in global_summary["intermediate_steps"]]
-        qa_chain = load_qa_chain(llm, chain_type="stuff")
-        query = "What are characteristics that {0} and {1} like about each other and quotes (include quotations) they've said to each other".format(character_first, character_second)
-        additional_context = qa_chain({"input_documents": doc_summaries, "question": query}, return_only_outputs=True)
-        return global_summary["output_text"] + additional_context["output_text"]
+        if (len(global_summary["output_text"]) > 400):
+            return global_summary["output_text"]
+        else:
+            # To augment the summary with more details that don't get lost, we extract some info from the summaries
+            doc_summaries = [Document(page_content=summary) for summary in global_summary["intermediate_steps"]]
+            print(global_summary["intermediate_steps"])
+            qa_chain = load_qa_chain(llm, chain_type="stuff")
+            query = "What are characteristics that {0} and {1} like about each other and quotes (include quotations) they've said to each other".format(character_first, character_second)
+            additional_context = qa_chain({"input_documents": doc_summaries, "question": query}, return_only_outputs=True)
+            return global_summary["output_text"] + additional_context["output_text"]
     except Exception as e:
         print("Error generating summary: ", e)
         raise e
