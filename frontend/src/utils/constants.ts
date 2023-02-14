35371601577a292574dcae33a6f6c2c8eecf1aa1
amirgamil <amir.amgad01@gmail.com>
2023-02-13 21:26:49 -0500

    feat(frontend): add loading messages

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = "https://api.sweetserenade.xyz/create-song"
+export const API_ENDPOINT = "https://api.sweetserenade.xyz/create-song";
 export const TEMPLATE_SONG = `Verse 1:
 Listen up people, this story's real
 Of two hearts in love that's beyond the steel
@@ -23,3 +23,14 @@ Their love was strong, but their world took flight
 They both had to leave, their fate was sealed
 But Yasmin and Raj, their love was revealed
 `;
+
+export const loadingTextOptions = [
+    "Generating a love song...",
+    "Downloading more sappiness... 0% complete",
+    "Assembling the love song puzzle piece by piece... one moment please",
+    "Sifting through countless love ballads... just for you",
+    "Compiling all the cheesiest love lyrics... almost there",
+    "Preparing the ultimate love anthem... brace yourself for the sap",
+    "Making sure the song is just the right amount of corny... loading",
+    "Love takes time, patience....",
+];
